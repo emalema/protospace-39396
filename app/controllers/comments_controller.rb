cfd6b5c0631ff2d5535a7dda0785f09a6de8c974
emalema <eyemalema5@gmail.com>
2023-08-29 14:09:02 +0900

    修正依頼箇所の修正②

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      puts @comment.errors.full_messages
-      @prototype = Prototype.find(params[:prototype_id])
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
